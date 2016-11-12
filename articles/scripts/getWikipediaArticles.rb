9fe42bb7cbaebe150e1cafbc70dca12d04697e87
Erik Thiem <erikthiem@gmail.com>
2016-11-12 14:08:59 -0600

    Added script to download random wikipedia summaries in order to create more test data.

---
+++ articles/scripts/getWikipediaArticles.rb
@@ -0,0 +1,31 @@
+require 'wikipedia'
+
+# This script gets summaries from random wikipedia articles
+
+# Search Spanish-language Wikipedia
+Wikipedia.Configure {
+    domain 'es.wikipedia.org'
+}
+
+number_of_articles = 10
+
+articles = []
+
+# Get the articles from Wikipedia
+for i in 0..number_of_articles-1 do
+
+    doc = Wikipedia.find_random()
+
+    article = {}
+
+	paragraphs = doc.summary.split("\n").reject { |s| s.empty? }
+
+    article[:title] = doc.title
+    article[:body] = paragraphs
+    articles << article
+end
+
+# Save the articles to a json file
+File.open("../data/articles_wikipedia.json", "w") do |f|
+    f.write(articles.to_json)
+end
